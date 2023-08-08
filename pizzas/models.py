bc4e2ea88d1e849b84981484fe1d17f4a80c4df4
Bella Ball <mohawkball@gmail.com>
2023-08-08 00:26:16 -0500

    created models

--- pizzas/models.py
+++ pizzas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Pizza(models.Model):
+    pizza_name = models.CharField(max_length=25)
+
+class Topping(models.Model):
+    topping_name = models.CharField(max_length=50)
+    pizza = models.ForeignKey(Pizza)
