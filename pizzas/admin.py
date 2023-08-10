308f3c53e8a78eed4e4533ea185f428e1edcd7f0
Bella Ball <mohawkball@gmail.com>
2023-08-10 01:30:51 -0500

    registered to admin, created pizzas and toppings

--- pizzas/admin.py
+++ pizzas/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from pizzas.models import Pizza, Topping
 
 # Register your models here.
+admin.site.register(Topping)
+admin.site.register(Pizza)
--- pizzas/models.py
+++ pizzas/models.py
@@ -3,7 +3,11 @@ from django.db import models
 # Create your models here.
 class Pizza(models.Model):
     pizza_name = models.CharField(max_length=25)
+    def __str__(self):
+        return self.pizza_name
 
 class Topping(models.Model):
     topping_name = models.CharField(max_length=50)
-    pizza = models.ForeignKey(Pizza)
+    pizza = models.ForeignKey(Pizza, on_delete=models.DO_NOTHING)
+    def __str__(self):
+        return self.topping_name
