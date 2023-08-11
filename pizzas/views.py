5c891090ef31bb99caa63f9e9aa9c0682eb74966
Bella Ball <mohawkball@gmail.com>
2023-08-11 01:12:36 -0500

    basic pizza listings page

--- pizzas/views.py
+++ pizzas/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
     return render(request, 'pizzas/index.html')
+
+def pizzas(request):
+    pizzas =Pizza.objects.all()
+
+    context ={'all_pizzas': pizzas}
+    return render(request, 'pizzas/pizzas.html', context)
